72ab5e748c95bac2d150970e56e7d0979d0c73c7
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-02-28 17:58:40 +0300

    Первые шаги

--- src/main/java/ru/geekbrains/WebServer.java
+++ src/main/java/ru/geekbrains/WebServer.java
@@ -1,7 +1,17 @@
 package ru.geekbrains;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class WebServer {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        try (ServerSocket serverSocket = new ServerSocket(8088)) {
+            Socket socket = serverSocket.accept();
+
+            System.out.println("New client connected");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
