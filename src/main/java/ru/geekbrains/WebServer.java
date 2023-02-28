1bcea48ef53de695c53e8dd775207d111774c069
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-02-28 18:10:46 +0300

    Вывод данных при коннекте

--- src/main/java/ru/geekbrains/WebServer.java
+++ src/main/java/ru/geekbrains/WebServer.java
@@ -1,15 +1,29 @@
 package ru.geekbrains;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class WebServer {
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(8088)) {
             Socket socket = serverSocket.accept();
-
             System.out.println("New client connected");
+
+            BufferedReader input = new BufferedReader(
+                    new InputStreamReader(
+                            socket.getInputStream(), StandardCharsets.UTF_8
+                    )
+            );
+
+            while(!input.ready());
+
+            while (input.ready()) {
+                System.out.println(input.readLine());
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
